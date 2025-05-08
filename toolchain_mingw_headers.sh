26a4cbf504ff082bcfd5e3d5d6869bdfb3fef583
Rahul Chandra <rahul@projecteclipse.org>
2025-05-07 23:17:37 -0400

    add wget to mingw-headers

--- toolchain_mingw_headers.sh
+++ toolchain_mingw_headers.sh
@@ -7,6 +7,7 @@ pkg=mingw
 # Untar
 
 cd $BUILD_DIR
+wget https://sourceforge.net/projects/$pkg-w64/files/$pkg-w64/$pkg-w64-release/$pkg-w64-v$MINGW_VER.tar.bz2
 tar -xvf $BUILD_DIR/$pkg-w64-v$MINGW_VER.tar.bz2
 cd $pkg-w64-v$MINGW_VER
 
