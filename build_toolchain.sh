c96943cc4f50bb9f1c58c489a89dbb14c89268af
Rahul Chandra <rahul@projecteclipse.org>
2025-05-07 23:11:18 -0400

    Proper logging

--- build_toolchain.sh
+++ build_toolchain.sh
@@ -8,20 +8,20 @@ error_check() {
 	fi
 }
 source toolchain.conf
-
+mkdir -p $BUILD_DIR/logs
 echo "Starting build for $ARCH with $LIBC, the triplet is $TARGET_CROSS"
 
 echo "Building Binutils..."
-bash toolchain_binutils.sh &> /dev/null
+bash toolchain_binutils.sh &> $BUILD_DIR/logs/binutils.log
 error_check "$?"
 echo "Building mingw headers"
-bash toolchain_mingw_headers.sh &> /dev/null
+bash toolchain_mingw_headers.sh &> $BUILD_DIR/logs/mingw-headers.log
 echo "Building GCC Stage 1..."
-bash toolchain_gcc_static.sh &> /dev/null
+bash toolchain_gcc_static.sh &> $BUILD_DIR/logs/gcc-static.log
 error_check "$?"
 echo "Building mingw"
-bash toolchain_mingw.sh &> /dev/null
+bash toolchain_mingw.sh &> $BUILD_DIR/logs/mingw.log
 echo "Building the final stage of GCC..."
-bash toolchain_gcc_final.sh &> /dev/null
+bash toolchain_gcc_final.sh &> $BUILD_DIR/logs/gcc-final.log
 error_check "$?"
 echo "Run package.sh"
